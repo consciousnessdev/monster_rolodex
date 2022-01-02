339ccc5dad8ea4166b757593d784e50181c3464d
consciousness_dev <consciousness.dev@gmail.com>
2022-01-02 16:00:22 +0700

    note: - exercise answer

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './card.styles.css';
 
 const Card = ({ id, name, email }) => {
-    console.log(id);
-    return (
-      <div className="card-container">
-        <img alt="" src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-        <h1>{name}</h1>
-        <p>{email}</p>
-      </div>
-    );
+  /**
+   * Break Down Component bertujuan agar tiap komponen memiliki perhatian statenya masing2
+   * sehingga fleksibel dapat di-reusable
+   */
+  return (
+    <div className="card-container">
+      <img alt="" src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+      <h1>{name}</h1>
+      <p>{email}</p>
+    </div>
+  );
 }
 
 export default Card;
