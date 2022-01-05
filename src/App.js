2dffb39cc60aac71eccbfe205fba464410925eec
consciousness_dev <consciousness.dev@gmail.com>
2022-01-05 12:16:34 +0700

    note: Asynchronous State

--- src/App.js
+++ src/App.js
@@ -2,34 +2,43 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 class App extends Component {
-
-  constructor() {
-    super();
+  // note: if need passed props within constructor, must passed props
+  constructor(props) {
+    super(props);
 
     this.state = {
-      string: 'Hello Yihua'
-    }
+      meaningOfLife: 47,
+    };
   }
 
+  // another state declaration, because of babel & CRA
+  // state = {}
+
+  handleClick = () => {
+    /**
+     * NOTE: state update is synchronous, but second parameter is asynchronous
+     */
+    // synchronouse
+    this.setState((prevState, prevProps) => ({
+      // passed prevState & prevProps
+      meaningOfLife: prevState.meaningOfLife + prevProps.increment,
+    }), () => {
+      // asynchronous
+      console.log(this.state.meaningOfLife);
+    });
+  };
+
   render() {
-    const { string } = this.state;
+    const { meaningOfLife } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {
-              string
-            }
-          </p>
-          <button
-            onClick={() => this.setState({ string: 'Hello Andrei' })}
-          >
-            Change Text
-          </button>
+          <p>{meaningOfLife}</p>
+          <button onClick={this.handleClick}>Update State</button>
         </header>
       </div>
-    )
+    );
   }
 }
 
